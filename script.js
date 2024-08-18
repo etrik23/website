36e9d3c9c90a4efbae273b58bee01e9b5d72e187
etrik23 <emmanueldelacruzjuarez866@gmail.com>
2024-08-17 20:13:35 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -43,32 +43,6 @@ let boton = document.querySelector(".play")
       console.log(`Reproduciendo: ${audioEtiqueta.src}`)
     }) }
 
-
-
-
-    {
-        let boton = document.querySelector(".play3")
-        let audioEtiqueta = document.querySelector("audio")
-    
-        boton.addEventListener("click", () => {
-          audioEtiqueta.setAttribute("src", "barbie.mp3")
-          audioEtiqueta.play()
-          console.log(`Reproduciendo: ${audioEtiqueta.src}`)
-        })}
-    
-        /*play3*/
-        {
-        let boton = document.querySelector(".stop3")
-        let audioEtiqueta = document.querySelector("audio")
-    
-        boton.addEventListener("click", () => {
-          audioEtiqueta.setAttribute("src", "#")
-          audioEtiqueta.play()
-          console.log(`Reproduciendo: ${audioEtiqueta.src}`)
-        }) }
-
-
-
         {
             let boton = document.querySelector(".play4")
             let audioEtiqueta = document.querySelector("audio")
@@ -113,24 +87,3 @@ let boton = document.querySelector(".play")
                   console.log(`Reproduciendo: ${audioEtiqueta.src}`)
                 }) }
 
-
-                {
-                    let boton = document.querySelector(".play6")
-                    let audioEtiqueta = document.querySelector("audio")
-                
-                    boton.addEventListener("click", () => {
-                      audioEtiqueta.setAttribute("src", "falta.mp3")
-                      audioEtiqueta.play()
-                      console.log(`Reproduciendo: ${audioEtiqueta.src}`)
-                    })}
-                
-                    /*play6*/
-                    {
-                    let boton = document.querySelector(".stop6")
-                    let audioEtiqueta = document.querySelector("audio")
-                
-                    boton.addEventListener("click", () => {
-                      audioEtiqueta.setAttribute("src", "#")
-                      audioEtiqueta.play()
-                      console.log(`Reproduciendo: ${audioEtiqueta.src}`)
-                    }) }
