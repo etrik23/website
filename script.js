8cc9b3efaa47df23e8c5b6db1f495f8f87409b45
etrik23 <emmanueldelacruzjuarez866@gmail.com>
2024-08-17 19:49:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+let boton = document.querySelector(".play")
+    let audioEtiqueta = document.querySelector("audio")
+
+    boton.addEventListener("click", () => {
+      audioEtiqueta.setAttribute("src", "secreto.mp3")
+      audioEtiqueta.play()
+      console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+    })
+
+    /*play1*/
+    {
+    let boton = document.querySelector(".stop")
+    let audioEtiqueta = document.querySelector("audio")
+
+    boton.addEventListener("click", () => {
+      audioEtiqueta.setAttribute("src", "#")
+      audioEtiqueta.play()
+      console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+    }) }
+
+
+
+
+
+{
+    let boton = document.querySelector(".play2")
+    let audioEtiqueta = document.querySelector("audio")
+
+    boton.addEventListener("click", () => {
+      audioEtiqueta.setAttribute("src", "rosa.mp3")
+      audioEtiqueta.play()
+      console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+    })}
+
+    /*play2*/
+    {
+    let boton = document.querySelector(".stop2")
+    let audioEtiqueta = document.querySelector("audio")
+
+    boton.addEventListener("click", () => {
+      audioEtiqueta.setAttribute("src", "#")
+      audioEtiqueta.play()
+      console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+    }) }
+
+
+
+
+    {
+        let boton = document.querySelector(".play3")
+        let audioEtiqueta = document.querySelector("audio")
+    
+        boton.addEventListener("click", () => {
+          audioEtiqueta.setAttribute("src", "barbie.mp3")
+          audioEtiqueta.play()
+          console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+        })}
+    
+        /*play3*/
+        {
+        let boton = document.querySelector(".stop3")
+        let audioEtiqueta = document.querySelector("audio")
+    
+        boton.addEventListener("click", () => {
+          audioEtiqueta.setAttribute("src", "#")
+          audioEtiqueta.play()
+          console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+        }) }
+
+
+
+        {
+            let boton = document.querySelector(".play4")
+            let audioEtiqueta = document.querySelector("audio")
+        
+            boton.addEventListener("click", () => {
+              audioEtiqueta.setAttribute("src", "manos.mp3")
+              audioEtiqueta.play()
+              console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+            })}
+        
+            /*play4*/
+            {
+            let boton = document.querySelector(".stop4")
+            let audioEtiqueta = document.querySelector("audio")
+        
+            boton.addEventListener("click", () => {
+              audioEtiqueta.setAttribute("src", "#")
+              audioEtiqueta.play()
+              console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+            }) }
+
+
+
+            {
+                let boton = document.querySelector(".play5")
+                let audioEtiqueta = document.querySelector("audio")
+            
+                boton.addEventListener("click", () => {
+                  audioEtiqueta.setAttribute("src", "carcel.mp3")
+                  audioEtiqueta.play()
+                  console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+                })}
+            
+                /*play5*/
+                {
+                let boton = document.querySelector(".stop5")
+                let audioEtiqueta = document.querySelector("audio")
+            
+                boton.addEventListener("click", () => {
+                  audioEtiqueta.setAttribute("src", "#")
+                  audioEtiqueta.play()
+                  console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+                }) }
+
+
+                {
+                    let boton = document.querySelector(".play6")
+                    let audioEtiqueta = document.querySelector("audio")
+                
+                    boton.addEventListener("click", () => {
+                      audioEtiqueta.setAttribute("src", "falta.mp3")
+                      audioEtiqueta.play()
+                      console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+                    })}
+                
+                    /*play6*/
+                    {
+                    let boton = document.querySelector(".stop6")
+                    let audioEtiqueta = document.querySelector("audio")
+                
+                    boton.addEventListener("click", () => {
+                      audioEtiqueta.setAttribute("src", "#")
+                      audioEtiqueta.play()
+                      console.log(`Reproduciendo: ${audioEtiqueta.src}`)
+                    }) }
